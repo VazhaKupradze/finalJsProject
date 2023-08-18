3c214c8ca2b34588971578814a0736a3e9341413
VazhaKupradze <94189837+VazhaKupradze@users.noreply.github.com>
2023-08-18 16:49:37 +0400

    upd

---
+++ script.js
@@ -0,0 +1,35 @@
+let nextBtn = document.querySelector('.next-btn');
+let prevBtn = document.querySelector('.prev-btn');
+let slides = document.querySelectorAll('.slide');
+let numberOfSlides = slides.lenght;
+let slideNumber = 0;
+
+//next
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
+
+//prev
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber--;
+
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
